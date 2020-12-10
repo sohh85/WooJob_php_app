db419a0809afd950cb8856e3a07f9a8c0bf11884
sohh <sohh.akasaka85@gmail.com>
2020-12-10 17:54:37 +0900

    ファビコン追加

--- www/html/index.php
+++ www/html/index.php
@@ -47,6 +47,8 @@ if (!empty($_POST)) {
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous" />
   <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
   <link rel="stylesheet" type="text/css" href="css/style.css" />
+  <link rel="shortcut icon" href="images/favicon.png" type="image/vnd.microsoft.icon">
+  <link rel="icon" href="images/favicon.png" type="image/vnd.microsoft.icon">
 </head>
 
 <body>
