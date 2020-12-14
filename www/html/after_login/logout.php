0aba12d7c02dd53f29d501e48d62474e1cf6b801
sohh <sohh.akasaka85@gmail.com>
2020-12-15 00:32:12 +0900

    ログアウト時にメッセージ表示

--- www/html/after_login/logout.php
+++ www/html/after_login/logout.php
@@ -8,8 +8,8 @@ if (ini_get('session.use_cookies')) {
     setcookie(session_name(), '', time() - 42000, $params['path'], $params['domain'], $params['secure'], $params['httponly']);
 }
 
-session_destroy();
+// session_destroy();
 setcookie('email', '', time() - 3600);
 
-header('Location: ../index.php');
+header('Location: ../index.php?logout');
 exit();
