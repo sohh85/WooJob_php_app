58656c782033a346253ba394e42372d29013ea38
sohh <sohh.akasaka85@gmail.com>
2020-12-17 18:29:26 +0900

    検索ロジック内の変数、値の名前を正しく変更

--- www/html/job_find/job_find.php
+++ www/html/job_find/job_find.php
@@ -4,7 +4,8 @@ require_once '../function.php';
 // データ取得ロジック呼び出し
 include_once('model.php');
 
-$userData = getUserData($_GET);
+// フォームで選ばれた$_GETの値を関数使用し検索
+$jobData = getJobData($_GET);
 
 ?>
 <!DOCTYPE HTML>
@@ -24,34 +25,38 @@ $userData = getUserData($_GET);
 
         <!-- 条件検索フォーム  -->
         <form method="get">
+
             <div class="form-group">
-                <label for="InputName">企業・店の名前</label>
-                <input name="name" class="form-control" id="InputName" value="<?= isset($_GET['name']) ? h($_GET['name']) : '' ?>">
+                <label for="Name">企業・店の名前</label>
+                <input name="name" class="form-control" id="Name" value="<?= isset($_GET['name']) ? h($_GET['name']) : '' ?>">
             </div>
+
             <div class="form-group">
-                <label for="InputSex">地域</label>
-                <select name="sex" class="form-control" id="InputSex">
-                    <option value="0" <?= empty($_GET['sex']) ? 'selected' : '' ?>>選択しない</option>
-                    <option value="1" <?= isset($_GET['sex']) && $_GET['sex'] == '1' ? 'selected' : '' ?>>シドニー</option>
-                    <option value="2" <?= isset($_GET['sex']) && $_GET['sex'] == '2' ? 'selected' : '' ?>>メルボルン</option>
+                <label for="City">地域</label>
+                <select name="city" class="form-control" id="City">
+                    <option value="0" <?= empty($_GET['city']) ? 'selected' : '' ?>>選択しない</option>
+                    <option value="1" <?= isset($_GET['city']) && $_GET['city'] == '1' ? 'selected' : '' ?>>シドニー</option>
+                    <option value="2" <?= isset($_GET['city']) && $_GET['city'] == '2' ? 'selected' : '' ?>>メルボルン</option>
                 </select>
             </div>
+
             <div class="form-group">
                 <label for="Wage">時給</label>
                 <select name="wage" class="form-control" id="Wage">
                     <option value="0" <?= empty($_GET['wage']) ? 'selected' : '' ?>>選択しない</option>
-                    <option value="10" <?= isset($_GET['wage']) && $_GET['wage'] == '10' ? 'selected' : '' ?>>15ドル以上</option>
+                    <option value="15" <?= isset($_GET['wage']) && $_GET['wage'] == '15' ? 'selected' : '' ?>>15ドル以上</option>
                     <option value="20" <?= isset($_GET['wage']) && $_GET['wage'] == '20' ? 'selected' : '' ?>>20ドル以上</option>
-                    <option value="30" <?= isset($_GET['wage']) && $_GET['wage'] == '30' ? 'selected' : '' ?>>25ドル以上</option>
+                    <option value="25" <?= isset($_GET['wage']) && $_GET['wage'] == '25' ? 'selected' : '' ?>>25ドル以上</option>
                 </select>
             </div>
+
             <div class="form-group">
-                <label for="">英語環境</label>
-                <select name="age" class="form-control" id="InputAge">
-                    <option value="0" <?= empty($_GET['age']) ? 'selected' : '' ?>>選択しない</option>
-                    <option value="10" <?= isset($_GET['age']) && $_GET['age'] == '10' ? 'selected' : '' ?>>10代</option>
-                    <option value="20" <?= isset($_GET['age']) && $_GET['age'] == '20' ? 'selected' : '' ?>>20代</option>
-                    <option value="30" <?= isset($_GET['age']) && $_GET['age'] == '30' ? 'selected' : '' ?>>30代</option>
+                <label for="Lang">英語使用頻度</label>
+                <select name="lang" class="form-control" id="Lang">
+                    <option value="0" <?= empty($_GET['lang']) ? 'selected' : '' ?>>選択しない</option>
+                    <option value="1" <?= isset($_GET['lang']) && $_GET['lang'] == '1' ? 'selected' : '' ?>>ほぼない</option>
+                    <option value="2" <?= isset($_GET['lang']) && $_GET['lang'] == '2' ? 'selected' : '' ?>>たまに</option>
+                    <option value="3" <?= isset($_GET['lang']) && $_GET['lang'] == '3' ? 'selected' : '' ?>>頻繁に</option>
                 </select>
             </div>
 
@@ -63,8 +68,8 @@ $userData = getUserData($_GET);
 
     <div class="col-xs-6 col-xs-offset-3">
         <!-- ヒットしたデータを表示する  -->
-        <?php if (isset($userData) && count($userData)) : ?>
-            <p class="alert alert-success"><?= count($userData) ?>件見つかりました。</p>
+        <?php if (isset($jobData) && count($jobData)) : ?>
+            <p class="alert alert-success"><?= count($jobData) ?>件見つかりました。</p>
             <table class="table">
                 <thead>
                     <tr>
@@ -74,11 +79,12 @@ $userData = getUserData($_GET);
                     </tr>
                 </thead>
                 <tbody>
-                    <?php foreach ($userData as $row) : ?>
+                    <?php foreach ($jobData as $row) : ?>
                         <tr>
                             <td><?= h($row['name']) ?></td>
-                            <td><?= h($row['sex'] == 1 ? '男性' : '女性') ?></td>
-                            <td><?= h($row['age']) ?></td>
+                            <td><?= h($row['city']) ?></td>
+                            <td><?= h($row['wage']) ?></td>
+                            <td><?= h($row['lang']) ?></td>
                         </tr>
                     <?php endforeach; ?>
                 </tbody>
---
+++ www/html/job_find/model.php
@@ -0,0 +1,45 @@
+<?php
+
+// $_GETで渡された検索条件を$paramsに代入
+function getJobData($params)
+{
+    //DB接続情報
+    include_once('../pdo_connect.php');
+
+    //入力された検索条件から WHERE(○○ = ○○) ここに入るSQl文を生成
+    $where = [];
+    // 企業・店の名前
+    if (!empty($params['name'])) {
+        $where[] = "name like '%{$params['name']}%'";
+    }
+    // 地域
+    if (!empty($params['city'])) {
+        $where[] = 'city = ' . $params['city'];
+    }
+    // 時給
+    if (!empty($params['wage'])) {
+        $where[] = 'wage >= ' . (int)$params['wage'];
+    }
+    // 英語使用頻度
+    if (!empty($params['lang'])) {
+        $where[] = 'lang = ' . $params['lang'];
+    }
+
+    if ($where) {
+        // implode関数でSQL分をAND(第一引数)で連結
+        $whereSql = implode(' AND ', $where);
+        $sql = 'select * from job_data where ' . $whereSql;
+    } else {
+        $sql = 'select * from job_data';
+    }
+
+    //SQL文を実行する
+    $jobDataSet = $dbh->query($sql);
+
+    //扱いやすい形に変える
+    $result = [];
+    while ($row = $jobDataSet->fetch(PDO::FETCH_ASSOC)) {
+        $result[] = $row;
+    }
+    return $result;
+}
