e4277d3a8cc02ce0a4b956fbac14ae1c2415a9c0
sohh <sohh.akasaka85@gmail.com>
2020-12-19 20:51:11 +0900

    不要なpタグ内のテキストを削除

--- www/html/job_find/job_find.php
+++ www/html/job_find/job_find.php
@@ -157,17 +157,14 @@ $cities = array(1 => "シドニー", 2 => "メルボルン", 3 => "ケアンズ"
                             <p class="alert alert-danger">検索対象は見つかりませんでした。</p>
                         <?php endif; ?>
 
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
+                        <small class="text-muted">Posted by Anonymous on 3/1/17</small>
+                        <hr>
 
                         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
                         <small class="text-muted">Posted by Anonymous on 3/1/17</small>
                         <hr>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
-                        <small class="text-muted">Posted by Anonymous on 3/1/17</small>
-                        <hr>
-                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Omnis et enim aperiam inventore, similique necessitatibus neque non! Doloribus, modi sapiente laboriosam aperiam fugiat laborum. Sequi mollitia, necessitatibus quae sint natus.</p>
-                        <small class="text-muted">Posted by Anonymous on 3/1/17</small>
-                        <hr>
-                        <a href="#" class="btn btn-success">Leave a Review</a>
+
                     </div>
                 </div>
                 <!-- /.card -->
