65bee32b4654b784a9731f817158677a3c394130
sohh <sohh.akasaka85@gmail.com>
2020-12-21 00:40:47 +0900

    検索ページの横幅を小さめに調整

--- www/html/job_find/job_find.php
+++ www/html/job_find/job_find.php
@@ -53,9 +53,24 @@ $cities = array(1 => "シドニー", 2 => "メルボルン", 3 => "ケアンズ"
     </nav>
 
     <!-- Page Content -->
-    <div class="container">
-
+    <div class="container widthSize">
         <div class="row">
+
+            <div class="col-lg-12 my-5">
+                <h1 class="my-2">Shop Name</h1>
+
+                <div class="card">
+                    <img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt="">
+                    <div class="card-body">
+                        <h3 class="card-title">Product Name</h3>
+                        <h4>$24.99</h4>
+                        <p class="card-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sapiente dicta fugit fugiat hic aliquam itaque facere, soluta. Totam id dolores, sint aperiam sequi!</p>
+                        <span class="text-warning">&#9733; &#9733; &#9733; &#9733; &#9734;</span>
+                        4.0 stars
+                    </div>
+                </div>
+            </div>
+
 
             <div class="col-lg-4">
                 <div class="px-3">
@@ -114,10 +129,7 @@ $cities = array(1 => "シドニー", 2 => "メルボルン", 3 => "ケアンズ"
 
             <div class="col-lg-8">
 
-                <div class="card mt-4">
-                    <!-- 選んだ地域の画像を表示 -->
-                    <img class="card-img-top img-fluid" src="http://placehold.it/900x400" alt="">
-                </div>
+
                 <!-- /.card -->
 
                 <div class="card card-outline-secondary my-4">
