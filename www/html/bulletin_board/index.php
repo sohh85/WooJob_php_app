ad0a6532b02b782c1dbdddc7f65bb9b0475f6c3b
sohh <sohh.akasaka85@gmail.com>
2020-12-26 20:54:24 +0900

    popper.jsとbootstrap圧縮ファイル読み込みをログイン後のファイルに追加

--- www/html/bulletin_board/index.php
+++ www/html/bulletin_board/index.php
@@ -73,16 +73,20 @@ if (isset($_REQUEST['res'])) {
     <!-- ファビコン -->
     <link rel="shortcut icon" href="../images/favicon.png" type="image/vnd.microsoft.icon">
     <link rel="icon" href="../images/favicon.png" type="image/vnd.microsoft.icon">
+    <!-- フォントオーサム -->
+    <script src="https://kit.fontawesome.com/82342a278b.js" crossorigin="anonymous"></script>
 </head>
 
 <body>
     <!-- header -->
     <nav class="navbar navbar-expand-lg navbar-light bg-white fixed-top">
         <div class="container">
+
             <a class="navbar-brand" href="#"></a>
             <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
+
             <div class="collapse navbar-collapse" id="navbarResponsive">
                 <ul class="navbar-nav ml-auto">
 
@@ -199,7 +203,8 @@ if (isset($_REQUEST['res'])) {
     </footer>
     <!-- Optional JavaScript -->
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="https://kit.fontawesome.com/82342a278b.js" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
     <!-- URL有効化JSファイル -->
     <script src="../js/validate-url.js"></script>
 </body>
